365a71750dc24bc8a8df0c563d7a47ffbb4d5803
patrick <princenazio2@gmail.com>
2019-03-13 14:22:33 +0100

    chore(styles):Remove unneeded styles from the sass file

--- webpack.config.js
+++ webpack.config.js
@@ -35,6 +35,17 @@ module.exports = {
     extensions: ['*', '.js', '.jsx']
   },
   externals: {
-    react: 'commonjs react'
+    react: {
+      commonjs: 'react',
+      commonjs2: 'react',
+      amd: 'React',
+      root: 'React'
+    },
+    'react-dom': {
+      commonjs: 'react-dom',
+      commonjs2: 'react-dom',
+      amd: 'ReactDOM',
+      root: 'ReactDOM'
+    }
   }
 };

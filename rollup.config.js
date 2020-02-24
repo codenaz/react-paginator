08e66b575acb2885ad4fa1d1dcf9ac4badbba967
Patrick Nwakwoke <princenazio2@gmail.com>
2020-02-24 18:06:29 +0100

    chore(bundle):build with rollup

---
+++ rollup.config.js
@@ -0,0 +1,44 @@
+import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
+import external from 'rollup-plugin-peer-deps-external';
+import postcss from 'rollup-plugin-postcss';
+import resolve from 'rollup-plugin-node-resolve';
+import visualizer from 'rollup-plugin-visualizer';
+
+import pkg from './package.json';
+
+export default {
+  input: './src/lib/index.js',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs'
+    },
+    {
+      file: pkg.module,
+      format: 'esm'
+    }
+  ],
+  plugins: [
+    external(),
+    postcss({
+      extensions: ['.css', '.scss']
+    }),
+    babel({
+      exclude: 'node_modules/**'
+    }),
+    resolve(),
+    commonjs({
+      namedExports: {
+        'node_modules/prop-types/index.js': [
+          'number',
+          'string',
+          'func',
+          'oneOfType',
+          'node'
+        ]
+      }
+    }),
+    visualizer()
+  ]
+};

7814490c1c3bd9ea29f26dc6668a4d79761f1f8c
camilomontoyau <camilomontoya2@gmail.com>
2023-01-02 16:47:16 -0500

    @rollup/plugin-babel

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,4 @@
-import babel from 'rollup-plugin-babel';
+import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 import external from 'rollup-plugin-peer-deps-external';
 import postcss from 'rollup-plugin-postcss';

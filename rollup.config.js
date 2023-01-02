4a61e989cec0e1305d0fddd0626c6ca12af2cc99
camilomontoyau <camilomontoya2@gmail.com>
2023-01-02 16:50:36 -0500

    rollup-plugin-visualizer
    checkpoint

--- rollup.config.js
+++ rollup.config.js
@@ -3,12 +3,12 @@ import commonjs from '@rollup/plugin-commonjs';
 import external from 'rollup-plugin-peer-deps-external';
 import postcss from 'rollup-plugin-postcss';
 import resolve from '@rollup/plugin-node-resolve';
-import visualizer from 'rollup-plugin-visualizer';
+import { visualizer } from 'rollup-plugin-visualizer';
 import typescript from '@rollup/plugin-typescript';
-import { terser } from '@rollup/plugin-terser';
+import terser from '@rollup/plugin-terser';
 import dts from 'rollup-plugin-dts';
 
-import pkg from './package.json';
+import pkg from './package.json' assert { type: "json" };
 
 export default [
   {
